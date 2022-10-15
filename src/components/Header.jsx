32a706d49903223f3090b3b99d5606554ddce203
Daniel <acidistrict@gmail.com>
2022-10-15 21:02:56 +0100

    preparing project

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <div>
+          <Image />
+        </div>
+      </div>
+      <div></div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Head from "next/head";
+import Header from "../components/Header";
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>Amazon</title>
+      </Head>
+      <Header />
+    </div>
+  );
+};
+
+export default Home;
